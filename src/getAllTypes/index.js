2f892cf6236af52c693414280f52a5ebd31400e8
was <455258482@qq.com>
2020-06-13 16:23:40 +0800

    fixe: 类型检查增加了类型

--- src/getAllTypes/index.js
+++ src/getAllTypes/index.js
@@ -3,17 +3,21 @@ const toString = Object.prototype.toString
 /** `Object#toString` result references. */
 export const numberTag = '[object Number]'
 export const boolTag = '[object Boolean]'
+export const stringTag = '[object String]'
 export const argsTag = '[object Arguments]'
 export const arrayTag = '[object Array]'
 export const dateTag = '[object Date]'
 export const errorTag = '[object Error]'
-export const mapTag = '[object Map]'
 export const objectTag = '[object Object]'
 export const regexpTag = '[object RegExp]'
+export const mapTag = '[object Map]'
 export const setTag = '[object Set]'
-export const stringTag = '[object String]'
-export const symbolTag = '[object Symbol]'
+export const weakSetTag = '[object WeakSet]'
 export const weakMapTag = '[object WeakMap]'
+export const symbolTag = '[object Symbol]'
+export const arrayBufferTag = '[object ArrayBuffer]'
+export const dataViewTag = '[object DataView]'
+export const bolbTag = '[object Blob]'
 
 export function getAllTypes(value) {
   const tag = toString.call(value)
--- src/index.js
+++ src/index.js
@@ -11,9 +11,28 @@
 
 // import './各个异步执行顺序的问题/index'
 
-const map = new Map()
-map.add('name', 'zxt')
+// const map = new Map()
+// map.add('name', 'zxt')
 
-const promise = new Promise()
+// const promise = new Promise()
 
-const a = 'zhaoxuetong'
+// const a = 'zhaoxuetong'
+
+// const a = new ArrayBuffer(8)
+// console.log(Object.prototype.toString.call(a))
+
+// const b = new DataView(a)
+// console.log(Object.prototype.toString.call(b))
+
+// const c = new Int8Array(8)
+// console.log(Object.prototype.toString.call(c))
+
+// const d = new Blob()
+// console.log(Object.prototype.toString.call(d))
+
+console.log([] instanceof Array)
+console.log({} instanceof Array)
+console.log([] instanceof Object)
+console.log({} instanceof Object)
+
+
